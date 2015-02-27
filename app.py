715e0d5335ff11c0e0ded73681772cbcb2bfb3a5
Zachary Snyder <sadin@fedoraproject.org>
2015-02-27 11:28:08 -0500

    added some python logic into index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
+import json, requests
 
 app = Flask(__name__)
 
@@ -9,5 +9,8 @@ def index():
 
 @app.route('/whois', methods=['POST'])
 def whois():
-    return 'saved!'
+	return redirect(url_for('index'))
+
+	
+
 app.run(debug=True, host='0.0.0.0', port=8000)
